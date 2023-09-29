343d0e6b9deb463816b0208072d95ba9b25cc5d0
VAB712 <114145360+VAB712@users.noreply.github.com>
2023-09-29 16:52:01 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -25,25 +25,6 @@ const About = () => {
             <RiFindReplaceLine />
           </Link>
         </article>
-
-        <div>
-          <h2>Founders</h2>
-          <article>
-            <div>
-              <h3>Aditi</h3>
-              <h3>Ankita</h3>
-              <h3>Sudeep</h3>
-              <h3>Vaibhav</h3>
-              
-              
-
-            </div>
-
-            <p>
-            Hey, Everyone, This is our mini project for WAD LAB
-            </p>
-          </article>
-        </div>
       </main>
     </section>
   );
