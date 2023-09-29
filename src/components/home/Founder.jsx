0435195af328c32cf8e0d5d94ed76e98bba73e81
VAB712 <114145360+VAB712@users.noreply.github.com>
2023-09-29 16:59:16 +0530

    Update Founder.jsx

--- src/components/home/Founder.jsx
+++ src/components/home/Founder.jsx
@@ -16,17 +16,6 @@ const Founder = () => {
   return (
     <section className="founder">
       <motion.div {...options}>
-        <img src={me} alt="Founder" height={200} width={200} />
-        <h3>Aditi</h3>
-        <h3>Ankita</h3>
-        <h3>Sudeep</h3>
-        <h3>Vaibhav</h3>
-
-        <p>
-          Hey, Everyone, This is our mini project for WAD LAB
-          <br />
-          
-        </p>
       </motion.div>
     </section>
   );
